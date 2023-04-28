433f6aec6ba41ae33c739b7584e6f41a2d9d86f7
Ricardo Jose Guevara Aragon <ttwrick@gmail.com>
2023-04-28 14:03:58 -0600

    Added the correct content to the services area, still lacking a service page and calculator

--- src/app/main-page/index/index.component.ts
+++ src/app/main-page/index/index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IconDefinition, faGlobeAmericas, faThLarge, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition, faGlobeAmericas, faGraduationCap, faPenNib, faSearch, faThLarge, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-index',
@@ -10,19 +10,29 @@ export class IndexComponent implements OnInit {
 
   public services: ServicesModel[] = [
     {
-      icn: faThLarge,
-      title: 'Corrección',
-      content: 'Corrección en inglés de la mas alta calidad, hecha por un experto en su campo, para garantizar que su texto esté listo para enviarlo a la revista'
-    },
-    {
-      icn: faGlobeAmericas,
-      title: 'Traducción',
-      content: 'Traducción de su manuscrito del español a inglés, con calidad de publicación'
-    },
-    {
       icn: faUserFriends,
-      title: 'Formateo',
-      content: 'Formateo de las referencias y de todo el manuscrito según las especificaciones de la revista seleccionada'
+      title: 'Asesorías',
+      content: 'Podemos ayudarle con su proyecto de investigación desde la elección del tema, la pregunta de investigación, el protocolo, hasta el informe final, si fuera necesario'
+    },
+    {
+      icn: faSearch,
+      title: 'Edición/Corrección',
+      content: 'Nuestros expertos revisarán el lenguaje de su manuscrito asegurando la gramática, ortografía, precisión, claridad, brevedad, coherencia y consistencia'
+    },
+    {
+      icn: faThLarge,
+      title: 'Análisis de datos',
+      content: 'El equipo de análisis de datos le ayuda a analizar los datos de la mejor manera posible ya que son expertos en el área de estadeistica y de ciencias sociales'
+    },
+    {
+      icn: faPenNib,
+      title: 'Diseño gráfico',
+      content: 'Nuestros diseñadores gráficos te darán ideas y/o harán propuestas de los gráficos o imágenes que mejor reflejen el mensaje que quieres transmitir de acuerdo con tus posibles lectores.'
+    },
+    {
+      icn: faGraduationCap,
+      title: 'Cursos',
+      content: 'Si tu interés es hacer una carrera en investigación o prepararte para desarrollar tu propia investigación, nuestro equipo estará realizando cursos virtuales dos veces al año o por demanda de grupos específicos.'
     }
   ];
 
